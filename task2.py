f8ea40d4923b0bd88a5659213056922e50f5d40f
hungrybeagle <cyang@deltalearns.ca>
2026-01-11 21:49:24 -0800

    Break number guessing game into functions
    Refactor the number guessing game into functions for better organization.

--- task2.py
+++ task2.py
@@ -1,10 +1,12 @@
 """
-Task 2:
 Create a program to play a number guessing game
 There should be a function:
 title()
 displays instructions and how to play
 
 game()
-plays the games
+plays the game
+
+This will be silimar to something you have already done, but in this task you 
+are breaking the code up into different sections to make each a function.
 """
