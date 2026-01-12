faf9455de5edc700fdad188e1ff3d20c4d6d308a
hungrybeagle <cyang@deltalearns.ca>
2026-01-11 21:47:57 -0800

    Implement functions A, B, and C in task1.py
    Define three functions to print messages based on user input.

--- task1.py
+++ task1.py
@@ -1,6 +1,15 @@
+"""
+Create a program with 3 function definitions:
+function A prints the message "Hello"
+function B prints the message "How are you"
+function C prints the message "Hi"
+
+Ask the user to enter a letter from A to C
+Execute the function of the letter they use.
+"""
+
 def A():
 
 def B():
-
 
 def C():
