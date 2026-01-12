75549347cf3f19df0afd6b77c246e472c63d300e
hungrybeagle <cyang@deltalearns.ca>
2026-01-11 21:45:48 -0800

    Add documentation for example3c.py
    Add comments explaining the use of __name__ == '__main__'

---
+++ example3c.py
@@ -0,0 +1,11 @@
+#python3
+#example3, example3a, example3b and example3d go together to illustrate the point of using
+# if__name__ == "__main__":
+
+import example3
+#you will notice that this program imports the entire file from example3.py but does not call the function. 
+#That's because the function all is inside a conditional statement:
+#
+#if __name__ == "__main__":
+#
+# THIS program is the MAIN program, and the imported example3 is in the "example3" program.
