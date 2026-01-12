fbe71668863e5a23e18f2f29cd6ddd506537c7c7
hungrybeagle <cyang@deltalearns.ca>
2026-01-11 21:37:27 -0800

    Add comment about function execution
    Added a comment explaining that the function is not called.

--- example1.py
+++ example1.py
@@ -3,4 +3,5 @@
 def sayHelloWorld():
     print("hello world")
 
+# You will notice that program doesn't do anything because even though the function has been defined, there is no command to CALL or EXECUTE the function.
 
