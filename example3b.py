bb2d9b493d3d213facf84e89bb3bba36e5aca7d5
hungrybeagle <cyang@deltalearns.ca>
2026-01-11 21:42:13 -0800

    Update example3b.py

--- example3b.py
+++ example3b.py
@@ -1,4 +1,7 @@
 #python3
+#example3, example3a, example3b and example3d go together to illustrate the point of using
+# if__name__ == "__main__":
 
 import example3
-
+#you will notice that this program imports the entire file from example3.py and calls the function that executes the code to say "Hello world".  
+#Importing the file imports all of the function definitions as well as any code that is not in a function
